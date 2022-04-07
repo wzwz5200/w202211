a17b6f2790f8dc3c0da08f49111968fa1363115c
wzwz5200 <80612049+wzwz5200@users.noreply.github.com>
2022-04-07 08:45:47 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "a062f436-4073-441f-88f6-c123951f07d8",
                         "alterId": 0
                     }
                 ],
